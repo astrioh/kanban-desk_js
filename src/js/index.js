f955df98657e2a250698b744ab4b970fe269afaa
AstRioh <korova674@gmail.com>
2020-03-21 04:02:19 +0800

    add switch between tasks, name changing and deletion

--- src/js/index.js
+++ src/js/index.js
@@ -6,23 +6,27 @@ class MainFrame {
     addTaskList(type) {
         let taskList = new TaskList(type);
         this.taskLists.push(taskList);
-        const hook = document.querySelector('.task-wrapper__add');
-        taskList.renderTaskList(type, hook);
+        const hook = document.querySelector('.task-wrapper__main');
+        let taskListEl = taskList.renderTaskList(type);
+        hook.append(taskListEl);
         console.log(this.taskLists);
     }
-    updateUI(type) {
-        document.querySelector('.task-wrapper__main').remove();
-        let mainFrame = document.createElement('div');
-        mainFrame.classList.add('.task-wrapper__main');
-        for (let taskList of this.taskLists) {
-            let taskListEl = new TaskList(type);
-            taskListEl.renderTaskList(type, document.querySelector('.task-wrapper__add'));
-            for (let task of taskList.tasks) {
-                if (task.type === type) {
 
-                }
+    updateUI(type) {
+        let mainFrame = document.querySelector('.task-wrapper__main');
+        let taskLists = mainFrame.querySelectorAll('.task-list');
+
+        taskLists.forEach(taskList => {
+            taskList.querySelectorAll('.task-list__task').forEach(task => task.style.display = 'none');
+            taskList.querySelectorAll(`.task-list__task_${type}`).forEach(task => task.style.display = 'flex');
+
+            const addTaskBtn = taskList.querySelector('.task-list__add');
+            if (type === 'finished') {
+                addTaskBtn.style.display = 'none';
+            } else {
+                addTaskBtn.style.display = 'block';
             }
-        }
+        });
     }
 }
 
@@ -31,65 +35,123 @@ class TaskList {
         this.type = type;
         this.tasks = [];
     }
-    addTask(name, hook) {
-        let task = new Task(name);
-        this.tasks.push(task);
-        task.renderTaskEl('active', hook);
+    
+    clearDeleted() {
+        this.tasks.forEach((task, index) => {
+            if (task.type === 'deleted') {
+                this.tasks.splice(index, 1);
+            }
+        });
     }
-    renderTaskList(type, hook) {
+
+    addTask(name, hook) {
+        let task = new Task('active', name);
+        this.tasks.push(task);
+        let taskEl = task.renderTaskEl();
+        hook.prepend(taskEl);
+        this.clearDeleted();
+    }
+    
+    renderTaskList(type) {
         let taskListEl = document.createElement('div');
+
         taskListEl.innerHTML = `
             <div class="task-list task-list_${type}">
-                <div class="task-list__header">
+                <div class="task-list__header task-list__header_${type}">
                     <h2 class="task-list__title">Sample name</h2>
                     <img class="task-list__edit" src="./img/edit.png" alt="edit">
                 </div>
                 <ul class="task-list__tasks"></ul>
             </div>`;
+
+        const editTaskListTitle = taskListEl.querySelector('.task-list__edit');
+        editTaskListTitle.addEventListener('click', e => {
+            let text = prompt('Введите название списка: ');
+            e.currentTarget.parentNode.querySelector('.task-list__title').textContent = text;
+        });
+        
         if (type === 'active') {
             let addBtn = document.createElement('li');
             addBtn.innerHTML = `<button class="add-btn task-list__add">+</button>`;
             
             addBtn.addEventListener('click', e => {
                 let name = prompt('Enter task name: ');
-                let hook = e.currentTarget;
+                let hook = e.currentTarget.parentNode;
                 this.addTask(name, hook);
             });
             
             taskListEl.querySelector('.task-list__tasks').append(addBtn);
         }
-        hook.before(taskListEl);
+        return taskListEl;
     }
 }
 
 class Task {
-    constructor(name) {
-        this.type = 'active';
+    constructor(type, name) {
+        this.type = type;
         this.name = name;
     }
-    renderTaskEl(type, hook) {
+
+    renderTaskEl() {
         let taskEl = document.createElement('li');
+
         taskEl.innerHTML = `
-            <li class="task-list__task">
+            <li class="task-list__task task-list__task_${this.type}">
                 <div>
                     <input id="do-${this.name}" type="checkbox" class="task-list__check">
                     <label for="do-${this.name}" class="task-list__description">${this.name}</label>
                 </div>
                 <div>
-                    <button>Edit</button>
-                    <button>Del</button>
+                    <button class="task-list__edit-task">Edit</button>
+                    <button class="task-list__delete-task">Del</button>
                 </div>
             </li>
         `;
-        taskEl.querySelector('.task-list__check').addEventListener('click', e => {
-            this.type = 'finished';
-            e.currentTarget.parentNode.parentNode.remove();
+
+        const checkBtn = taskEl.querySelector('.task-list__check');
+        checkBtn.addEventListener('click', e => {
+            if (this.type === 'active') {
+                this.type = 'finished';
+                e.currentTarget.parentNode.parentNode.classList = 'task-list__task task-list__task_finished';
+                e.currentTarget.parentNode.parentNode.style.display = 'none';
+            }
         });
-        hook.before(taskEl);
+        
+        const editBtn = taskEl.querySelector('.task-list__edit-task');
+        editBtn.addEventListener('click', e => {
+            let text = prompt('Введите новое название: ');
+            this.name = text;
+            e.currentTarget.parentNode.parentNode.querySelector('.task-list__description').textContent = text;
+        });
+
+        const deleteBtn = taskEl.querySelector('.task-list__delete-task');
+        deleteBtn.addEventListener('click', e => {
+            e.currentTarget.parentNode.parentNode.remove();
+            this.type = 'deleted';
+        });
+
+        return taskEl;
     }
 }
 
-let mainFrame = new MainFrame('active');
+const mainFrame = new MainFrame('active');
+
 mainFrame.addTaskList('active');
-let addTaskListBtn = document.querySelector('.task-wrapper__add');
+
+const addTaskListBtn = document.querySelector('.task-wrapper__add'),
+    activeBtn = document.querySelector('.task-wrapper__switch-active'),
+    finishedBtn = document.querySelector('.task-wrapper__switch-finished');
+
 addTaskListBtn.addEventListener('click', mainFrame.addTaskList.bind(mainFrame, 'active'));
+
+activeBtn.addEventListener('click', e => {
+    activeBtn.classList.add('task-wrapper__switch-btn_selected');
+    finishedBtn.classList.remove('task-wrapper__switch-btn_selected');
+    mainFrame.updateUI('active');
+});
+
+finishedBtn.addEventListener('click', e => {
+    finishedBtn.classList.add('task-wrapper__switch-btn_selected');
+    activeBtn.classList.remove('task-wrapper__switch-btn_selected');
+    mainFrame.updateUI('finished');
+});
